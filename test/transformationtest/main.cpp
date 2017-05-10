7ae4b3b98db4d35fa1c71c8667c6b7c9a49bbd09
Jan Knedlik <j.knedlik@gsi.de>
2017-05-10 17:31:08 +0200

    [tests] transformation test now added points near the boundaries

--- test/transformationtest/main.cpp
+++ test/transformationtest/main.cpp
@@ -10,63 +10,36 @@
 #include <map>
 #include <utility>
 using namespace MinA;
-/*
-template <size_t N>
-using TA = typename TArray<double, N>::type;
-template <size_t N>
-TA<N> TranslateVal(TA<N> params, typename Boundarytuple<TA<N>>::type bounds,
-                   bool retransform)
-{
- TA<N> newparams;
- if (!retransform) {
-  for_each_tuple(params, newparams, bounds,
-                 [](auto& param, auto& newparam, auto& bound) {
-                  double a = bound.left;
-                  double b = bound.right;
-                  newparam = (a + b - 2.0 * param) / (a - b);
-                  if (newparam < 0)
-                   newparam = newparam / (1.0 - newparam);
-                  else
-                   newparam = newparam / (1.0 + newparam);
 
-                 });
- }
- else {
-  for_each_tuple(params, newparams, bounds,
-                 [](auto& param, auto& newparam, auto& bound) {
-                  double a = bound.left;
-                  double b = bound.right;
-
-                  if (param < 0)
-                   newparam = param / (1 + param);
-                  else
-                   newparam = param / (1 - param);
-                  newparam = a + (b - a) * (1 + newparam) / 2.0;
-
-                 });
- }
- return newparams;
-}
-*/
 int main(int argc, char** argv)
 {
  Camel6 f;
  auto v = f.startvalues;
  auto b = f.bounds;
+ auto vx = f.startvalues;
+ auto b1 = std::get<0>(f.bounds).left;
+ auto b2 = std::get<1>(f.bounds).right;
+ auto crit = ((b2 - b1) * 0.01) / 4.f;
+ std::get<0>(vx) = b1 + crit;
+ std::get<1>(vx) = b2 - crit;
  // typename Boundarytuple<TA<3>>::type b{ { 10, 100 }, { 0, 10 }, { 300, 400 }
  // };
  std::cout << "\n"
            << "Pre Transform:" << std::endl;
  for_each_tuple(v, [](auto& p) { std::cout << p << ","; });
+ for_each_tuple(vx, [](auto& p) { std::cout << p << ","; });
  v = TranslateVal<2>(v, b, false);
+ vx = TranslateVal<2>(vx, b, false);
  std::cout << "\n"
            << "Post Transform:" << std::endl;
  for_each_tuple(v, [](auto& p) { std::cout << p << ","; });
+ for_each_tuple(vx, [](auto& p) { std::cout << p << ","; });
  v = TranslateVal<2>(v, b, true);
-
+ vx = TranslateVal<2>(vx, b, true);
  std::cout << "\n"
            << "Post ReTransform:" << std::endl;
  for_each_tuple(v, [](auto& p) { std::cout << p << ","; });
+ for_each_tuple(vx, [](auto& p) { std::cout << p << ","; });
 
  return (0);
 }
